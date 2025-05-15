449c49401b08991ef62e14d5438715910d88af65
Salvatore <salvatorefranzese99@gmail.com>
2025-05-15 10:03:27 +0200

    Character Blueprint Setup

--- Source/Aura/Private/Character/BaseCharacter.cpp
+++ Source/Aura/Private/Character/BaseCharacter.cpp
@@ -7,6 +7,9 @@ ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ABaseCharacter::BeginPlay()
