0bc072bf191208ee7b78c7cab05894116b7df4eb
Salvatore <salvatorefranzese99@gmail.com>
2025-05-21 09:53:15 +0200

    Ability System Component and Attribute Set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Salvatore Franzese
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Salvatore Franzese
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
