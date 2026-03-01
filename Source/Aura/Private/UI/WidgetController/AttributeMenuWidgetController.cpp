f97dab94562ecb0609c78fd4854225a9b22a2d2e
Salvatore <salvatorefranzese99@gmail.com>
2026-03-01 19:29:17 +0100

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,15 @@
+// Copyright Salvatore Franzese
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
